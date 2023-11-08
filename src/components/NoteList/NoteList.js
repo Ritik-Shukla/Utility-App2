6b751934eda82948d49b18f0aee6ceddaf44d93b
Ritik <shuklaritik731@gmail.com>
2023-11-09 00:26:39 +0530

    implementation of fething state data while loading of the app by making HTTP calls through components

---
+++ src/components/NoteList/NoteList.js
@@ -0,0 +1,30 @@
+
+import { useSelector, useDispatch } from "react-redux";
+// import { deleteNote } from "../../redux/actions/noteActions";
+import { actions } from "../../redux/reducers/noteReducer";
+import { noteSelector } from "../../redux/reducers/noteReducer";
+import styles from "./NoteList.module.css";
+
+function NoteList() {
+    const notes=useSelector(noteSelector);
+    console.log(notes);
+    const dispatch=useDispatch();
+
+  return (
+    <div className={styles.container}>
+    <ul>
+      {notes.map((note,index) => (
+        <li className={styles.item} key={note.id || index}>
+            <p>{new Date(note.createdOn).toLocaleDateString()}</p>
+            <p className={styles.notecontent}>{note.text}</p>
+            <button className={"btn btn-danger " + styles.button} onClick={() => dispatch(actions.delete(index))}>
+  Delete
+</button>
+            </li>
+      ))}
+    </ul>
+    </div>
+  );
+}
+
+export default NoteList;
---
+++ src/redux/reducers/todoReducer.js
@@ -0,0 +1,78 @@
+const { createSlice } = require("@reduxjs/toolkit")
+
+
+const initialState={
+    todos:[
+    ]
+}
+
+// Creating Reducer using Redux Toolkit
+
+const todoSlice = createSlice({
+    name:'todo',
+    initialState:initialState,
+    reducers:{
+    
+        setInitialState:(stte,action)=>{
+           stte.todos=[...action.payload]
+        },
+
+
+
+        // this is add action
+        add:(state, action)=>{
+                state.todos.push({
+                    text:action.payload,
+                    completed: false
+                })
+        },
+        toggle:(state, action)=>{
+            state.todos.map((todo, i)=>{
+                if(i==action.payload){
+                    todo.completed=!todo.completed;
+                }
+                return todo;
+            })
+        }
+    }
+});
+
+export const todoReducer=todoSlice.reducer;
+
+export const actions = todoSlice.actions;
+
+// selector
+export const todoSelector = (state)=>state.todoReducer.todos;
+
+
+
+// Reducer using redux
+
+// export function todoReducer(state=initialState, action){
+
+//     switch(action.type){
+//         case ADD_TODO:
+//             return {
+//                 ...state,
+//                 todos:[
+//                     ...state.todos,
+//                     {
+//                         text:action.text,
+//                         completed: false
+//                     }
+//                 ]
+//             }
+//         case TOGGLE_TODO:
+//             return{
+//                 ...state,
+//                 todos: state.todos.map((todo, i)=>{
+//                     if(i==action.index){
+//                         todo.completed=!todo.completed
+//                     }
+//                     return todo;
+//                 })
+//             }
+//         default:
+//             return state;
+//     }
+// }
